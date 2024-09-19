0bcaa2ccb08a7e4c01794b28f61fc101ed42fe4c
Carlos C. Romero Pertuz <jpuello@meico.com.co>
2024-09-19 22:10:43 +0000

    Se añaden Credenciales de autenticación Azure AD

--- src/environments/enviroments.ts
+++ src/environments/enviroments.ts
@@ -2,8 +2,8 @@ export const environment = {
   apiAuthUrl: "http://localhost:5299/",
   apiAppUrl: "http://localhost:5299/",
   azure: {
-    clientId: '',
-    authority: 'https://login.microsoftonline.com/addTenantId',
+    clientId: 'a91c9d2f-a0e0-478b-a265-4a1da3d441a3',
+    authority: 'https://login.microsoftonline.com/a06d2c92-7803-417f-9812-16af600fee47',
     loginRedirectUri: 'http://localhost:4200/',
     logoutRedirectUri: 'http://localhost:4200/',
     scopes: ['User.Read'],
